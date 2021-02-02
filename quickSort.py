dc6c992d31ec2fcfd90fde2d5a32fd9168a08fdd
baiyongjun <baiyongjun@xiaoniangao.com>
2021-02-02 23:32:03 +0800

    Feature: add quick sort
    Signed-off-by: baiyongjun <baiyongjun@xiaoniangao.com>

---
+++ quickSort.py
@@ -0,0 +1,11 @@
+def quick_sort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print(quick_sort([3, 8, 5, 6, 8, 5, 3, 5, 7, 0]))
