6534cddccf7b3d240e4b94675d2b31619869dae4
baiyongjun <byj994@163.com>
2021-05-11 17:05:07 +0800

    Add length of long string
    Signed-off-by: baiyongjun <byj994@163.com>

---
+++ lengthOfLongestSubstring.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "dfsffdsafdzsx"
+	fmt.Println(lenthOfLongestSubstring(s))
+}
+
+func lenthOfLongestSubstring(s string) (length int) {
+	if s == "" {
+		return
+	}
+
+	length = 0
+	for i := 0; i < len(s); i++ {
+		aa := map[byte]bool{}
+		aa[s[i]] = true
+		for j := i + 1; j < len(s); j++ {
+			if _, ok := aa[s[j]]; ok {
+				break
+			}
+			aa[s[j]] = true
+			sMax := j - i + 1
+			if sMax > length {
+				length = sMax
+			}
+		}
+	}
+	return
+}
